a74cc88c9fa8928c0c462f9098a3c9c08a8db21c
NikitoG <niki_spike@yahoo.com>
2015-08-18 23:29:36 +0300

    HQC - Development tools - homework

---
+++ Hight-Quality-Code/DevelopmentToolsHomework/ResearchAndUseTools/Log4net/Log4netExample/Log4netTest.cs
@@ -0,0 +1,23 @@
+﻿namespace Log4netExample
+{
+    using System;
+
+    using log4net;
+    using log4net.Config;
+
+    class Log4netTest
+    {
+        private static readonly ILog Log =
+          LogManager.GetLogger(typeof(Log4netTest));
+        static void Main()
+        {
+            BasicConfigurator.Configure();
+            Log.Debug("Debug msg");
+            Log.Error("Error msg");
+            Log.Fatal("Fatal msg");
+            Log.Warn("Warn msg");
+            Log.Info("Info msg");
+        }
+
+    }
+}
---
+++ Hight-Quality-Code/DevelopmentToolsHomework/T4TamplateTest/T4Template.cs
@@ -0,0 +1,19 @@
+﻿
+public class Month
+{
+    public void MonthAsWord()
+    {
+        var month1 = "January";
+        var month2 = "February";
+        var month3 = "March";
+        var month4 = "April";
+        var month5 = "May";
+        var month6 = "June";
+        var month7 = "July";
+        var month8 = "August";
+        var month9 = "September";
+        var month10 = "October";
+        var month11 = "November";
+        var month12 = "December";
+    }
+}
