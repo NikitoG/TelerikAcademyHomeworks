f106c2ce003b7ae8349d5321ae522ba201afa6bc
NikitoG <niki_spike@yahoo.com>
2015-08-18 21:10:14 +0300

    HQC - Variables data expressions and constants - homework

---
+++ Hight-Quality-Code/VariablesDataExpressionsAndConstantsHomework/ArrayStatistics.cs
@@ -0,0 +1,52 @@
+﻿namespace VariablesDataExpressionsAndConstantsHomework
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class ArrayStatistics
+    {
+        public void PrintStatistics(double[] sequence)
+        {
+            double maxElement = this.GetMax(sequence);
+            double minElement = this.GetMin(sequence);
+            double average = this.GetAverage(sequence);
+
+            StringBuilder statistics = new StringBuilder();
+            statistics.AppendLine(string.Format("Max: {0}", maxElement));
+            statistics.AppendLine(string.Format("Min: {0}", minElement));
+            statistics.AppendLine(string.Format("Average: {0}", average));
+
+            Console.WriteLine(statistics);
+        }
+
+        public double GetMax(double[] sequence)
+        {
+            double maxElement = double.MinValue;
+
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                if (sequence[i] > maxElement)
+                {
+                    maxElement = sequence[i];
+                }
+            }
+
+            return maxElement;
+        }
+
+        public double GetMin(double[] sequence)
+        {
+            double minElement = sequence.Min();
+
+            return minElement;
+        }
+
+        public double GetAverage(double[] sequence)
+        {
+            double average = sequence.Average();
+
+            return average;
+        }
+    }
+}
---
+++ Hight-Quality-Code/VariablesDataExpressionsAndConstantsHomework/Figure.cs
@@ -0,0 +1,64 @@
+﻿namespace VariablesDataExpressionsAndConstantsHomework
+{
+    using System;
+
+    public class Figure
+    {
+        private double height;
+        private double width;
+
+        public Figure(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Height must be greater than zero!");
+                }
+
+                this.height = value;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Width must be greater than zero!");
+                }
+
+                this.width = value;
+            }
+        }
+
+        public static Figure GetRotatedFigure(Figure figure, double angleOfTheFigureThatWillBeRotaed)
+        {
+            double rotatedFigureWidth = (Math.Abs(Math.Cos(angleOfTheFigureThatWillBeRotaed)) * figure.Width) +
+                                        (Math.Abs(Math.Sin(angleOfTheFigureThatWillBeRotaed)) * figure.Height);
+            double rotatedFigureHeigth = (Math.Abs(Math.Sin(angleOfTheFigureThatWillBeRotaed)) * figure.Width) +
+                                         (Math.Abs(Math.Cos(angleOfTheFigureThatWillBeRotaed)) * figure.Height);
+
+            Figure figureAfterRotated = new Figure(rotatedFigureWidth, rotatedFigureHeigth);
+
+            return figureAfterRotated;
+        }
+    }
+}
