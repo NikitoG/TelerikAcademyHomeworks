0d772e5e9948712b9c609d9d9541407d97d66171
NikitoG <niki_spike@yahoo.com>
2015-08-19 11:22:23 +0300

    C Sharp I - Conditional statements - homework

---
+++ C#-I-Homework/ConditionalStatementsHomework/BonusScore/Bonus.cs
@@ -0,0 +1,38 @@
+﻿//Problem 2. Bonus Score
+
+//Write a program that applies bonus score to given score in the range [1…9] by the following rules:
+//If the score is between 1 and 3, the program multiplies it by 10.
+//If the score is between 4 and 6, the program multiplies it by 100.
+//If the score is between 7 and 9, the program multiplies it by 1000.
+//If the score is 0 or more than 9, the program prints “invalid score”.
+
+using System;
+
+class Bonus
+{
+    static void Main()
+    {
+        int score;
+        do
+        {
+            Console.Write("Enter a score in the range (1....9): ");
+        } while (!int.TryParse(Console.ReadLine(), out score));
+
+        if (0 < score && score < 4)
+        {
+            Console.WriteLine("Score {0} -> after bonus result is: {1}", score, score * 10);
+        }
+        else if (3 < score && score < 7)
+        {
+            Console.WriteLine("Score {0} -> after bonus result is: {1}", score, score * 100);
+        }
+        else if (6 < score && score < 10)
+        {
+            Console.WriteLine("Score {0} -> after bonus result is: {1}", score, score * 1000);
+        }
+        else
+        {
+            Console.WriteLine("Invalid score!");
+        }
+    }
+}
---
+++ C#-I-Homework/ConditionalStatementsHomework/ZeroSubset/ZeroSubset.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+class ZeroSubset
+{
+    static void Main()
+    {
+        Console.WriteLine("Write 5 ineger number(use \"Enter\" for separate them )!");
+        int[] fiveNumbers = {
+                                    int.Parse(Console.ReadLine()),
+                                    int.Parse(Console.ReadLine()),
+                                    int.Parse(Console.ReadLine()),
+                                    int.Parse(Console.ReadLine()),
+                                    int.Parse(Console.ReadLine())
+                                };
+        int count = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            for (int k = i + 1; k < 5; k++)
+            {
+                if ((fiveNumbers[i] + fiveNumbers[k]) == 0)
+                {
+                    Console.WriteLine("{0} + {1} = 0", fiveNumbers[i], fiveNumbers[k]);
+                    count++;
+                }
+            }
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            for (int k = i + 1; k < 4; k++)
+            {
+                for (int n = k + 1; n < 5; n++)
+                {
+                    if ((fiveNumbers[i] + fiveNumbers[k] + fiveNumbers[n]) == 0)
+                    {
+                        Console.WriteLine("{0} + {1} + {2} = 0", fiveNumbers[i], fiveNumbers[k], fiveNumbers[n]);
+                        count++;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < 2; i++)
+        {
+            for (int k = i + 1; k < 3; k++)
+            {
+                for (int n = k + 1; n < 4; n++)
+                {
+                    for (int m = n + 1; m < 5; m++)
+                    {
+                        if ((fiveNumbers[i] + fiveNumbers[k] + fiveNumbers[n] + fiveNumbers[m]) == 0)
+                        {
+                            Console.WriteLine("{0} + {1} + {2} + {3} = 0", fiveNumbers[i], fiveNumbers[k], fiveNumbers[n], fiveNumbers[m]);
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+
+        if ((fiveNumbers[0] + fiveNumbers[1] + fiveNumbers[2] + fiveNumbers[3] + fiveNumbers[3]) + fiveNumbers[4] == 0)
+        {
+            Console.WriteLine("{0} + {1} + {2} + {3} +{4} = 0", fiveNumbers[0], fiveNumbers[1], fiveNumbers[2], fiveNumbers[3], fiveNumbers[4]);
+            count++;
+        }
+
+        if (count == 0)
+        {
+            Console.WriteLine("no zero subset");
+        }
+    }
+}
