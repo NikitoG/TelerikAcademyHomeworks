937522618ce7c5a992877b11719ffcda9244974d
NikitoG <niki_spike@yahoo.com>
2015-08-19 11:36:59 +0300

    C Sharp II - Exception handling - homework

---
+++ C#-II-Homework/ExceptionHandling/DownloadFile/Download.cs
@@ -0,0 +1,43 @@
+﻿//Problem 4. Download file
+
+//Write a program that downloads a file from Internet (e.g. Ninja image) and stores it the current directory.
+//Find in Google how to download files in C#.
+//Be sure to catch all exceptions and to free any used resources in the finally block.
+
+using System;
+using System.IO;
+using System.Net;
+
+class Download
+{
+    static void Main()
+    {
+        string address = @"http://telerikacademy.com/Content/Images/news-img01.png";
+        string fileName = "ninja.png";
+        using (WebClient web = new WebClient())
+        {
+            try
+            {
+                web.DownloadFile(address, fileName);
+                Console.WriteLine("Picture was download succesfully in: \n{0}", Directory.GetCurrentDirectory());
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (NotSupportedException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Good bye.");
+            }
+        }
+    }
+}
+
---
+++ C#-II-Homework/ExceptionHandling/ReadFileContents/ReadFile.cs
@@ -0,0 +1,64 @@
+﻿//Problem 3. Read file contents
+
+//Write a program that enters file name along with its full file path (e.g. C:\WINDOWS\win.ini), reads its contents and prints it on the console.
+//Find in MSDN how to use System.IO.File.ReadAllText(…).
+//Be sure to catch all possible exceptions and print user-friendly error messages.
+
+using System;
+using System.IO;
+
+class ReadFile
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a full file path: ");
+        string filePath = Console.ReadLine();
+        try
+        {
+            string fileContent = File.ReadAllText(filePath);
+            Console.WriteLine(fileContent);
+        }
+        catch (ArgumentNullException ex)
+        {
+
+            Console.WriteLine(ex.Message);
+        }
+        catch (ArgumentException ex)
+        {
+
+            Console.WriteLine(ex.Message);
+        }
+        catch (DirectoryNotFoundException ex)
+        {
+
+            Console.WriteLine(ex.Message);
+        }
+        catch (PathTooLongException ex)
+        {
+
+            Console.WriteLine(ex.Message);
+        }
+        catch (FileNotFoundException ex)
+        {
+
+            Console.WriteLine(ex.Message);
+        }
+        catch (IOException ex)
+        {
+
+            Console.WriteLine(ex.Message);
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+
+            Console.WriteLine(ex.Message);
+        }
+        catch (NotSupportedException ex)
+        {
+
+            Console.WriteLine(ex.Message);
+        }
+
+    }
+}
+
