c036279c2fbb9848fd3db738b4404db6cd0fb4ff
NikitoG <niki_spike@yahoo.com>
2015-11-06 01:34:55 +0200

    Jedi Meditation - WorkShop

---
+++ DSA/JediMeditation/Startup.cs
@@ -0,0 +1,43 @@
+﻿namespace JediMeditation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var jedis = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var knight = new Queue<string>();
+            var padawan = new Queue<string>();
+
+            var sb = new StringBuilder();
+
+            foreach (var jedi in jedis)
+            {
+                switch (jedi[0])
+                {
+                    case 'm':
+                    case 'M':
+                        sb.AppendFormat(jedi + " ");
+                        break;
+                    case 'k':
+                    case 'K':
+                        knight.Enqueue(jedi);
+                        break;
+                    case 'p':
+                    case 'P':
+                        padawan.Enqueue(jedi);
+                        break;
+                }
+            }
+
+            sb.AppendFormat(string.Join(" ", knight));
+            sb.Append(" ");
+            sb.AppendFormat(string.Join(" ", padawan));
+            Console.Write(sb);
+        }
+    }
+}
