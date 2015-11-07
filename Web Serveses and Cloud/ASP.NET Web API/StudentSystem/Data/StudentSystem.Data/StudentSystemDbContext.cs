a75beed65f2d2943d55c412ff157872adacbcf4f
NikitoG <niki_spike@yahoo.com>
2015-11-08 01:03:17 +0200

    ASP.NET Web API intro - Homework - Student System - Initialize

---
+++ Web Serveses and Cloud/ASP.NET Web API/StudentSystem/Data/StudentSystem.Data/StudentSystemDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace StudentSystem.Data
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using StudentSystem.Models;
+
+    public class StudentSystemDbContext : IdentityDbContext<User>
+    {
+        public StudentSystemDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static StudentSystemDbContext Create()
+        {
+            return new StudentSystemDbContext();
+        }
+    }
+}
